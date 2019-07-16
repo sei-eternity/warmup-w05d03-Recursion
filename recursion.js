3d345cafb6487d5416a821e35082f71496c4546b
alanoud-GA <alanoud.alrasheed@generalassemb.ly>
2019-07-16 11:13:58 +0300

    solution

--- recursion.js
+++ recursion.js
@@ -2,34 +2,69 @@
 // You will have to figure out what parameters to include
 // All functions must use recursion
 
-function findMax() {
-    // This function returns the largest number in a given array.
+
+function findMax(arr, index = 1, max) {
+    if (!max & arr.length) {
+        max = arr[0];
+    }
+    if (index == arr.length) {
+        return max;
+    } else {
+        if (arr[index] > max) {
+            max = arr[index]
+        }
+        return findMax(arr, index + 1, max);
+    }
 }
 
-function factorial() {
-    // This function returns the factorial of a given number.
+function factorial(num) {
+    if (num == 1 || num == 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
 }
 
-function fibonacci() {
-    // This function returns the Nth number in the fibonacci sequence.
-    // https://en.wikipedia.org/wiki/Fibonacci_number
-    // For this function, the first two fibonacci numbers are 1 and 1
+function fibonacci(num) {
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-function coinFlips() {
-    // This function returns an array of all possible outcomes from flipping a coin N times.
-    // Input type: Integer
-    // For example, coinFlips(2) would return the following:
-    // ["HH", "HT", "TH", "TT"]
-    // H stands for Heads and T stands for tails
-    // Represent the two outcomes of each flip as "H" or "T"
+function coinFlips(num) {
+    const results = [];
+
+    function rCoinFlips(stem = "") {
+        if (stem.length === num) {
+            results.push(stem);
+        } else {
+            rCoinFlips(stem + "H");
+            rCoinFlips(stem + "T");
+        }
+    }
+    rCoinFlips();
+    return results;
 }
 
-function letterCombinations() {
-    // This function returns an array of all combinations of the given letters
-    // Input type: Array of single characters
-    // For example, letterCombinations(["a","b","c"]) would return the following:
-    // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+function letterCombinations(arr) {
+    const results = [];
+
+    function rLetterCombinations(stem, remainder) {
+        if (stem.length === arr.length) {
+            results.push(stem);
+            return;
+        }
+        if (stem.length > 0) {
+            results.push(stem);
+        }
+        for (let i = 0; i < remainder.length; i++) {
+            const newRemainder = remainder.slice(0, i).concat(remainder.slice(i + 1));
+            rLetterCombinations(stem + remainder[i], newRemainder)
+        }
+    };
+    rLetterCombinations("", arr);
+    return results;
 }
 
 /////////////////////////TESTING//////////////////////
